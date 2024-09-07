2512dbb70aebd0916810d6fa373f4d5ff1b8c35f
unknown <ceedriclazaro010205@gmail.com>
2024-09-06 22:22:31 -0800

    Initial commit with app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+let isActive = true;
+let inSubscribed = false;
+console.log(isActive);
+console.log(inSubscribed);
+
+let x = 4;
+let y = 9;
+let isGreater = x > y;
+let isEqual = x == y;
+console.log(isGreater);
+console.log(isEqual);
